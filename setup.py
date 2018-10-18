7ea4051cf08fbd516188e3e19d357f3d4dcfad18
Ezra Peisach <ezra.peisach@rcsb.org>
2018-10-18 09:13:07 -0400

    Install wf-defs

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # Update:
 #
 import re
+import glob
 
 from setuptools import find_packages
 from setuptools import setup
@@ -20,7 +21,8 @@ if not version:
 
 # We need data for bootstrap
 #datadir = os.path.join('share','data','wf_engine_utils')
-#datafiles = 
+#datafiles =
+dfiles = glob.glob('wwpdb/apps/wf_engine/wf-defs/*.xml')
 
 setup(
     name=thisPackage,
@@ -58,9 +60,10 @@ setup(
     },
     data_files = [
         # Data for bootstrap
-        ('data/wf_engine', ['wwpdb/apps/wf_engine/wf_engine_utils/test/test-pdb-codes.ids',
-                                  'wwpdb/apps/wf_engine/wf_engine_utils/test/test-emdb-codes.ids',
-                                  'wwpdb/apps/wf_engine/wf_engine_utils/test/test-bmrb-codes.ids'])
+        ('data/wf_engine/bootstrap', ['wwpdb/apps/wf_engine/wf_engine_utils/test/test-pdb-codes.ids',
+                                      'wwpdb/apps/wf_engine/wf_engine_utils/test/test-emdb-codes.ids',
+                                      'wwpdb/apps/wf_engine/wf_engine_utils/test/test-bmrb-codes.ids']),
+        ('data/wf_engine/wf-defs', dfiles)
     ],
         
     #
